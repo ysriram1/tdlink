003bbe7838aaab49dd748d11c6e66d80f23ecdb6
ysriram1 <sriram.yarla1@gmail.com>
2019-07-02 15:12:34 -0400

    added more to setup params

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(name='tdlink',
       version='0.1',
       description='Python Library for TDAmeritrade API',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/ysriram1/tdlink',
       author='Sriram Yarlagadda',
       author_email='ysriram@umich.edu',
       license='MIT',
       packages=['tdlink'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ]
       zip_safe=False)

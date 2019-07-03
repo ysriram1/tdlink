0d135c772ac0ac4dd9248a0eec86474089dfea39
ysriram1 <sriram.yarla1@gmail.com>
2019-07-02 23:57:13 -0400

    incremeneted to v0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='tdlink',
-      version='0.2',
+      version='0.3',
       description='Python Library for TDAmeritrade API',
       long_description=long_description,
       long_description_content_type="text/markdown",
